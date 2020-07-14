7ef32b6d93bc9e7e6aab3b897032a448e5df78c0
ethanae <eemandien@mweb.co.za>
2020-07-14 22:12:20 +0200

    - simple message sender - add message ack fn

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import socket
 import argparse
-from confluent_kafka import Producer
 from enum import Enum
+from confluent_kafka import Producer
 
 def main():
     args = get_args()
-    print(args)
     producer = Producer({
         'bootstrap.servers': args.brokers,
         'client.id': socket.gethostname()
     })
     if (args.operation is Operation.SEND_MESSAGE):
-        print('producer.produce()')
+        producer.produce(topic=args.topic, value=args.message, callback=message_ack)
+        producer.poll(1)
 
+def message_ack(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
 
 class Operation(Enum):
     SEND_MESSAGE = 'send-message'
@@ -23,6 +28,10 @@ def get_args():
                         help='The operation to carry out')
     parser.add_argument('--brokers', type=str, default='', required=True,
                         help='A comma-separated list of broker addresses')
+    parser.add_argument('--message', type=str, default='',
+                        help='A JSON string of the message to send')
+    parser.add_argument('--topic', type=str, default='',
+                        help='The topic to which to send the message')
     return parser.parse_args()    
 
 if __name__ == "__main__":
